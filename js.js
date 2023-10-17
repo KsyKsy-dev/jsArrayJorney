b4af1a73a0aadb15577a896c0e00e919e13ae310
KsyKsy-dev <ksy.codedev@gmail.com>
2023-10-17 20:36:28 +0200

    write a camelize function

--- js.js
+++ js.js
@@ -1,4 +1,18 @@
 "use strict";
 /// create new array
 // constractor
-let arr = new Array();
+//let arr = new Array();
+
+// array literal
+// let arr = [];
+
+// create arr from string
+
+// let str = "I'm str";
+// let strArr = str.split(" ");
+// console.log(strArr);
+
+//#1 camelize(str)
+
+function camelize(str) {}
+console.log(camelize("my_string"));
